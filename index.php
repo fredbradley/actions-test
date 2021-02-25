06f05e425ea22d0984705532e48a798214413972
Fred Bradley <hello@fredbradley.co.uk>
2021-02-25 09:44:09 +0000

    refactor(docs): add documentation

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <?php
 
+/**
+ * @param  string  $string
+ *
+ * @return string
+ */
 function test(string $string): string
 {
     return $string;
 }
 
+/**
+ * @param  int  $int
+ *
+ * @return int
+ */
 function testAgain(int $int): int
 {
     return $int;
 }
 
+/**
+ * @param $var
+ *
+ * @return bool
+ */
 function isInt($var): bool
 {
     if ($var instanceof int) {
