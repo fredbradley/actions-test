cd4b554fffe8cdad71b4efb7121fc9f1a27acdb1
Fred Bradley <hello@fredbradley.co.uk>
2021-02-25 09:43:32 +0000

    refactor: if statement

--- index.php
+++ index.php
@@ -4,12 +4,16 @@ function test(string $string): string
 {
     return $string;
 }
+
 function testAgain(int $int): int
 {
     return $int;
 }
 
-function isInt($var):bool
+function isInt($var): bool
 {
-    return true;
+    if ($var instanceof int) {
+        return true;
+    }
+    return false;
 }
